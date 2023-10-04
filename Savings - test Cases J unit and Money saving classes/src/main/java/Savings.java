f3b86ee5e9c6e99d8702e00206e5727692d0099e
marialopesc <75050357+marialopesc@users.noreply.github.com>
2023-10-04 00:09:24 -0500

    Update and rename HomeworkOne.java to Savings.java

--- Savings - test Cases J unit and Money saving classes/src/main/java/Savings.java
+++ Savings - test Cases J unit and Money saving classes/src/main/java/Savings.java
@@ -2,7 +2,7 @@
 /*
  * This class will eventually contain the user interface
  */
-public class HomeworkOne {
+public class Savings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
